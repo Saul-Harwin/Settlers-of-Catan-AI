836b1db3935d1c3b3f27b30ce7abbb4a4fb06ada
Saul Harwin <saulharwin@gmail.com>
2026-02-17 17:27:48 +0000

    Sorted Conflicts in main.py

--- src/main.py
+++ src/main.py
@@ -11,27 +11,14 @@ board = generate_board(rng)
 
 players = [PlayerState() for _ in range(4)]
 
-<<<<<<< Updated upstream
 robber_hex = np.where(board.hex_terrain == 0)[0][0]
 print(robber_hex)
-=======
-robber_hex = rng.randint(0, 18)
->>>>>>> Stashed changes
 turn = 0
 
 game_state = GameState(board, players, robber_hex, turn, rng)
 
-<<<<<<< Updated upstream
-# simulate 5 turns
-game_states = simulate_game(game_state, n_turns=9, visualise=False)
-
-
-# visualise final state
-draw([game_states[-1]])
-=======
 # simulate n turns
 game_state = simulate_game(game_state, n_turns=500, visualise=False)
 print(game_state)
 
 draw(game_state)
->>>>>>> Stashed changes
